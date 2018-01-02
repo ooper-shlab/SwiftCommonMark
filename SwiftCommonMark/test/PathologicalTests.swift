7fd8dd892514cf6cccead554c4ea621c71936115
OOPer <nagata@shlab.jp>
2018-01-02 14:58:21 +0900

    added .gitignore

---
+++ SwiftCommonMark/test/PathologicalTests.swift
@@ -0,0 +1,9 @@
+//
+//  PathologicalTests.swift
+//  SwiftCommonMarkTest
+//
+//  Created by 開発 on 2018/1/1.
+//  Copyright © 2018 OOPer (NAGATA, Atsuyuki). All rights reserved.
+//
+
+import Foundation
